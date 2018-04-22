d6a084ce88050ee2e4954b12b8e3f7f688c31049
jmendez <jorge.mendez.ortega@gmail.com>
2018-04-22 14:58:41 -0500

    [Update]

--- test/test.js
+++ test/test.js
@@ -6,4 +6,10 @@ describe("OrcaSlide", function(){
         const orca = OrcaSlide;
         expect(orca).to.be.an("function")
     })
+    
+    it("Probando utilidades", function() {
+        const data = {node1: [{},{item:5},{}]};
+        const request = OrcaSlide.existFields(data, "node1.1.item", "null");
+        expect(request).to.be.equals(5);
+    });
 });
