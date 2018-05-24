1fe32c537e26e9295a6a511c12f629928731ef4b
jmendez <jorge.mendez.ortega@gmail.com>
2018-05-23 22:28:06 -0500

    ADD: nueva funcion
    Se carga el calculo para identificar el movimiento del Swipe
    
    N/A

--- source/Utils.js
+++ source/Utils.js
@@ -140,6 +140,37 @@ class Utils {
 
         return CALLBACKS;
     }
+
+    /**
+     * Permite conseguir la direccion en la que se reliza el swipe.
+     *
+     * @param  {object} swipe configuracion base para el swipe.
+     * @return {string}
+     */
+    static getDirecctionSlide(swipe) {
+        const HZR_X1 = ((swipe.endX - swipe.min_x) > swipe.startX);
+        const HZR_X2 = ((swipe.endX + swipe.min_x) < swipe.startX);
+        const HZR_Y1 = (swipe.endY < (swipe.startY + swipe.max_y));
+        const HZR_Y2 = (swipe.startY > (swipe.endY - swipe.max_y));
+
+        const VERT_Y1 = ((swipe.endY - swipe.min_y) > swipe.startY);
+        const VERT_Y2 = ((swipe.endY + swipe.min_y) < swipe.startY);
+        const VERT_X1 = (swipe.endX < (swipe.startX + swipe.max_x));
+        const VERT_X2 = (swipe.startX > (swipe.endX - swipe.max_x));
+
+        const IS_HORIZONTAL = ((HZR_X1 || HZR_X2) && (HZR_Y1 && HZR_Y2));
+        const IS_VERTICAL = ((VERT_Y1 || VERT_Y2) && (VERT_X1 && VERT_X2));
+
+        let direction = "";
+
+        if (IS_HORIZONTAL) {
+            direction = (swipe.endX > swipe.startX) ? "right" : "left";
+        } else if (IS_VERTICAL) {
+            direction = (swipe.endY > swipe.startY) ? "bottom" : "top";
+        }
+
+        return direction;
+    }
 }
 
 export default Utils;
