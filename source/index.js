5d2d647bbc962e7041e0a5edc06f5a922d6d4ef9
Jorge Mendez Ortega <jorge.mendez.ortega@#gmail.com>
2023-02-24 12:42:09 -0600

    chore(orca): probando auto publicacion

--- source/index.js
+++ source/index.js
@@ -7,6 +7,7 @@ import Config from "./orcaConfig.json";
  */
 class initOrcaSlide {
     static set config(config) {
+        console.log("Estas corriendo con orcaSlide 1.0.4");
         this.buildOrcaStorage(config);
         document.onreadystatechange = () => {
             if (document.readyState === "complete") {
